43402fd53d543be47e89e6843c67edfa696acfbb
Jedrik97 <punick97@gmail.com>
2025-05-07 19:51:37 +0300

    update

--- Assets/Resources/GameSceneInstaller.cs
+++ Assets/Resources/GameSceneInstaller.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 using Zenject;
+using Unity.Cinemachine;
 
 public class GameSceneInstaller : MonoInstaller
 {
+    [Header("Player Setup")]
+    public GameObject playerPrefab;
+
+    public Transform spawnPoint;
+
+    [Header("Other Controllers")]
     public GameObject GameManager;
-    public GameObject Player;
+
     public GameObject PauseMenuController;
+    
     public GameObject SlotSelectController;
 
     public override void InstallBindings()
@@ -14,21 +22,37 @@ public class GameSceneInstaller : MonoInstaller
             .FromComponentInHierarchy(GameManager)
             .AsSingle()
             .Lazy();
-        Container.Bind<PlayerStats>()
-            .FromComponentInHierarchy(Player)
-            .AsSingle()
-            .Lazy();
-        Container.Bind<HealthPlayerController>()
-            .FromComponentInHierarchy(Player)
-            .AsSingle()
-            .Lazy();
+        
         Container.Bind<PauseMenuController>()
             .FromComponentInHierarchy(PauseMenuController)
             .AsSingle()
             .Lazy();
+        
         Container.Bind<SlotSelectController>()
             .FromComponentInHierarchy(SlotSelectController)
             .AsSingle()
             .Lazy();
+        
+        
+        var playerGO = Object.Instantiate(
+            playerPrefab,
+            spawnPoint.position,
+            spawnPoint.rotation
+        );
+        playerGO.SetActive(true);
+        
+        Container.Bind<PlayerStats>()
+            .FromComponentOn(playerGO)
+            .AsSingle();
+
+        Container.Bind<HealthPlayerController>()
+            .FromComponentOn(playerGO)
+            .AsSingle();
+
+        Container.Bind<CharacterController>()
+            .FromComponentOn(playerGO)
+            .AsSingle();
+        
+        Container.InjectGameObject(playerGO);
     }
 }
