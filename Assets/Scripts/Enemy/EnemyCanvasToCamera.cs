2d6c7854ec825f8d1f4e0c4c6424ee2b8215ff18
Jedrik97 <punick97@gmail.com>
2025-05-23 10:54:52 +0300

    update

--- Assets/Scripts/Enemy/EnemyCanvasToCamera.cs
+++ Assets/Scripts/Enemy/EnemyCanvasToCamera.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class EnemyCanvasToCamera : MonoBehaviour
 {
-    // Ссылка на камеру игрока (если камера всегда Main Camera, можно использовать Camera.main)
+    
     private Transform playerCamera;
 
     void Start()
     {
-        // Инициализируем ссылку на основную камеру
+        
         if (Camera.main != null)
         {
             playerCamera = Camera.main.transform;
@@ -25,7 +25,7 @@ public class EnemyCanvasToCamera : MonoBehaviour
             Vector3 direction = transform.position - playerCamera.position;
             Quaternion targetRotation = Quaternion.LookRotation(direction);
 
-            // Фиксируем вращение по оси X (например, оставляем Y-поворот)
+            
             targetRotation.x = 0;
             targetRotation.z = 0;
 
